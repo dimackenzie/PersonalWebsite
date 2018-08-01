e4665e7e4d420d3c55de3af0653f5c60a56a662d
Ian Mackenzie <ian.mackenzie74@gmail.com>
2018-08-01 10:03:24 -0600

    Add hidden input field

--- src/components/contact.js
+++ src/components/contact.js
@@ -19,6 +19,7 @@ const Contact = () => (
             data-netlify="true"
             data-netlify-honeypot="bot-field"
           >
+            <input type="hidden" name="form-name" value="contact" />
             <label hidden>
               Don’t fill this out: <input name="bot-field" />
             </label>
